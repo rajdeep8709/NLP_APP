64e976c8791547d8b86ab15a9c22887dc2f7253c
rajdeep8709@gmail.com <rajdeep8709@gmail.com>
2023-10-10 11:32:30 +0530

    test

--- db_build.py
+++ db_build.py
@@ -1,3 +1,33 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2f1877860c39a19e605e885200a1aaec5255b4a66696b9213a08499aec4bae5e
-size 1214
+# =========================
+#  Module: Vector DB Build
+# =========================
+import box
+import yaml
+from langchain.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.embeddings import HuggingFaceEmbeddings
+
+# Import config vars
+with open('config/config.yml', 'r', encoding='utf8') as ymlfile:
+    cfg = box.Box(yaml.safe_load(ymlfile))
+
+
+# Build vector database
+def run_db_build():
+    loader = DirectoryLoader(cfg.DATA_PATH,
+                             glob='*.pdf',
+                             loader_cls=PyPDFLoader)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=cfg.CHUNK_SIZE,
+                                                   chunk_overlap=cfg.CHUNK_OVERLAP)
+    texts = text_splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
+                                       model_kwargs={'device': 'cpu'})
+
+    vectorstore = FAISS.from_documents(texts, embeddings)
+    vectorstore.save_local(cfg.DB_FAISS_PATH)
+
+if __name__ == "__main__":
+    run_db_build()
